5f89644428a061b14d98bdf6b86b2b1513184176
schweigenderFlugel <facundoezcastro@gmail.com>
2023-08-26 01:29:08 -0300

    cors añadido

--- src/middlewares/cors.handler.js
+++ src/middlewares/cors.handler.js
@@ -1,4 +1,4 @@
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["http://localhost:3000", "http://localhost:5173"];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
